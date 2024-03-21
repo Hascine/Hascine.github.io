6e4eb553a0c34085d7594ae5e0010ba6904efa48
pbalqish <putri.balqis.h20@gmail.com>
2024-03-21 19:42:14 +0700

    database - pertama

--- script.js
+++ script.js
@@ -1,2 +1,57 @@
-//ini awalnya banget
-// backend
+const db_game = [
+  {
+    id: "001ML",
+    gameName: "Mobile Legends",
+    product: [
+      ["170 Diamonds", 43_000],
+      ["305 Diamonds", 79_000],
+      ["408 Diamonds", 100_000],
+      ["1048 Diamonds", 265_000],
+      ["2010 Diamonds", 470_000],
+    ],
+  },
+  {
+    id: "002FF",
+    gameName: "Free Fire",
+    product: [
+      ["170 Diamonds", 43_000],
+      ["305 Diamonds", 79_000],
+      ["408 Diamonds", 100_000],
+      ["1048 Diamonds", 265_000],
+      ["2010 Diamonds", 470_000],
+    ],
+  },
+  {
+    id: "003VL",
+    gameName: "Valorant",
+    product: [
+      ["170 Diamonds", 43_000],
+      ["305 Diamonds", 79_000],
+      ["408 Diamonds", 100_000],
+      ["1048 Diamonds", 265_000],
+      ["2010 Diamonds", 470_000],
+    ],
+  },
+  {
+    id: "004GI",
+    gameName: "Genshin Impact",
+    product: [
+      ["170 Diamonds", 43_000],
+      ["305 Diamonds", 79_000],
+      ["408 Diamonds", 100_000],
+      ["1048 Diamonds", 265_000],
+      ["2010 Diamonds", 470_000],
+    ],
+  },
+  {
+    id: "005PM",
+    gameName: "PUBG Mobile",
+    product: [
+      ["170 Diamonds", 43_000],
+      ["305 Diamonds", 79_000],
+      ["408 Diamonds", 100_000],
+      ["1048 Diamonds", 265_000],
+      ["2010 Diamonds", 470_000],
+    ],
+  },
+];
