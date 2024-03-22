fa95ec0473d60535ba121a414a13812d3fadabc9
pbalqish <putri.balqis.h20@gmail.com>
2024-03-22 08:52:31 +0700

    js baru

--- script.js
+++ script.js
@@ -1,59 +1,100 @@
-const db_game = [
+const product = [
   {
-    id: "001ML",
-    gameName: "Mobile Legends",
-    product: [
-      ["170 Diamonds", 43_000],
-      ["305 Diamonds", 79_000],
-      ["408 Diamonds", 100_000],
-      ["1048 Diamonds", 265_000],
-      ["2010 Diamonds", 470_000],
-    ],
+    id: 0,
+    image: "image/ML.webp",
+    title: "Mobile Legends",
+    price: 1_200_000,
   },
   {
-    id: "002FF",
-    gameName: "Free Fire",
-    product: [
-      ["170 Diamonds", 43_000],
-      ["305 Diamonds", 79_000],
-      ["408 Diamonds", 100_000],
-      ["1048 Diamonds", 265_000],
-      ["2010 Diamonds", 470_000],
-    ],
+    id: 1,
+    image: "image/VL.webp",
+    title: "Valorant",
+    price: 600_000,
   },
   {
-    id: "003LL",
-    gameName: "League of Legends",
-    product: [
-      ["170 Diamonds", 43_000],
-      ["305 Diamonds", 79_000],
-      ["408 Diamonds", 100_000],
-      ["1048 Diamonds", 265_000],
-      ["2010 Diamonds", 470_000],
-    ],
+    id: 2,
+    image: "image/LOL.webp",
+    title: "League of Legends",
+    price: 2_300_000,
   },
   {
-    id: "004GI",
-    gameName: "Genshin Impact",
-    product: [
-      ["170 Diamonds", 43_000],
-      ["305 Diamonds", 79_000],
-      ["408 Diamonds", 100_000],
-      ["1048 Diamonds", 265_000],
-      ["2010 Diamonds", 470_000],
-    ],
+    id: 3,
+    image: "image/CODM.webp",
+    title: "Call of Duty Mobile",
+    price: 100_000,
   },
   {
-    id: "005CM",
-    gameName: "Call of Duty Mobile",
-    product: [
-      ["170 Diamonds", 43_000],
-      ["305 Diamonds", 79_000],
-      ["408 Diamonds", 100_000],
-      ["1048 Diamonds", 265_000],
-      ["2010 Diamonds", 470_000],
-    ],
+    id: 4,
+    image: "image/GI.webp",
+    title: "Genshin Impact",
+    price: 100_000,
   },
 ];
+const categories = [
+  ...new Set(
+    product.map((item) => {
+      return item;
+    })
+  ),
+];
+let i = 0;
+document.getElementById("root").innerHTML = categories
+  .map((item) => {
+    var { image, title, price } = item;
+    return (
+      `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>Rp ${price}.00</h2>` +
+      "<button onclick='addtocart(" +
+      i++ +
+      ")'>Tambah Ke Keranjang</button>" +
+      `</div>
+        </div>`
+    );
+  })
+  .join("");
 
-let db_order = [];
+var cart = [];
+
+function addtocart(a) {
+  cart.push({ ...categories[a] });
+  displaycart();
+}
+function delElement(a) {
+  cart.splice(a, 1);
+  displaycart();
+}
+
+function displaycart() {
+  let j = 0,
+    total = 0;
+  document.getElementById("count").innerHTML = cart.length;
+  if (cart.length == 0) {
+    document.getElementById("cartItem").innerHTML =
+      "Keranjang lo masih kosong tuh, Ngab";
+    document.getElementById("total").innerHTML = "Rp " + 0 + ".00";
+  } else {
+    document.getElementById("cartItem").innerHTML = cart
+      .map((items) => {
+        var { image, title, price } = items;
+        total = total + price;
+        document.getElementById("total").innerHTML = "Rp " + total + ".00";
+        return (
+          `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>Rp ${price}.00</h2>` +
+          "<i class='fa-solid fa-trash' onclick='delElement(" +
+          j++ +
+          ")'></i></div>"
+        );
+      })
+      .join("");
+  }
+}
